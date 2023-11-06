ccccecd87ba51f1d508b49ac00d54a697e6e36a9
nahid <dewnahidhossain@gmail.com>
2023-11-07 01:39:18 +0600

    code re arrenged for scrolling

--- index.js
+++ index.js
@@ -1,22 +1,15 @@
-console.log("hello");
-
-
+// ######### scroll effect #########
 let contact=document.querySelector("#contact");
 let blog=document.querySelector("#blog");
 let portfolio=document.querySelector("#portfolio");
 let about=document.querySelector("#about");
-// document.querySelector(".contact-link").addEventListener("click",()=>{
-//     contact.scrollIntoView({behavior: "smooth"});
-    
-// });
+
 let navLinks=document.querySelectorAll(".nav-link");
 navLinks.forEach((link)=>{
     link.addEventListener("click",(e)=>{
         e.preventDefault();
-    //    console.log(link.innerHTML);
         if(link.innerHTML=="Contact"){
             contact.scrollIntoView({behavior:"smooth"});
-        
         }else if(link.innerHTML=="Blog"){
             blog.scrollIntoView({behavior:"smooth"});
         }else if(link.innerHTML=="Portfolio"){
@@ -25,5 +18,4 @@ navLinks.forEach((link)=>{
             about.scrollIntoView({behavior:"smooth"});
         }
     })
-
 })
